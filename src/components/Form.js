fc724e304f64d5228c329890da20f6840931fb16
Levy <levyng.ng@gmail.com>
2023-11-27 11:18:31 +0530

    first commit
    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,9 +13,44 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 const Form = () => {
+  const [leadName, setLeadName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [sale, setSale] = useState("");
   const [startDate, setStartDate] = useState(new Date());
+  const [options, setOptions] = useState("");
+  const [product, setProduct] = useState("");
+  const [note, setNote] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      leadName,
+      email,
+      phoneNumber,
+      address,
+      sale,
+      startDate,
+      options,
+      product,
+      note,
+    });
+    resetForm();
+  };
+  const resetForm = () => {
+    setLeadName("");
+    setEmail("");
+    setPhoneNumber("");
+    setAddress("");
+    setSale("");
+    setStartDate(new Date());
+    setOptions("");
+    setProduct("");
+    setNote("");
+  };
   return (
-    <form className="form-cont">
+    <form className="form-cont" onSubmit={handleSubmit}>
       <div className="form-headers">
         <h3>Add new lead form</h3>
         <button>Cancel</button>
@@ -23,36 +58,52 @@ const Form = () => {
       <div className="form-inputs-cont">
         <div className="label-cont">
           <label>Enter lead name</label>
-          <img src={starIcon} height={"5px"} />
+          <img src={starIcon} height={"5px"} alt="img-not-found" />
         </div>
 
         <div className="text-field-cont">
           <img src={icon} height={"15px"} />
-          <input placeholder="Enter lead name"></input>
+          <input
+            placeholder="Enter lead name"
+            value={leadName}
+            onChange={(e) => setLeadName(e.target.value)}
+          ></input>
         </div>
         <div className="label-cont">
           <label>Email ID</label>
-          <img src={starIcon} height={"5px"} />
+          <img src={starIcon} height={"5px"} alt="img-not-found" />
         </div>
         <div className="text-field-cont">
-          <img src={emailIcon} height={"15px"} />
-          <input placeholder="example@gmail.com"></input>
+          <img src={emailIcon} height={"15px"} alt="img-not-found" />
+          <input
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
         </div>
         <div className="label-cont">
           <label>Phone number</label>
-          <img src={starIcon} height={"5px"} />
+          <img src={starIcon} height={"5px"} alt="img-not-found" />
         </div>
         <div className="text-field-cont">
-          <img src={phoneIcon} height={"15px"} />
-          <input placeholder="+9198634588145"></input>
+          <img src={phoneIcon} height={"15px"} alt="img-not-found" />
+          <input
+            placeholder="+9198634588145"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          ></input>
         </div>
         <div className="label-cont">
           <label>Address</label>
           {/* <img src={starIcon} height={"5px"} /> */}
         </div>
         <div className="text-field-cont">
-          <img src={locationIcon} height={"15px"} />
-          <input placeholder="Gurugram,India"></input>
+          <img src={locationIcon} height={"15px"} alt="img-not-found" />
+          <input
+            placeholder="Gurugram,India"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          ></input>
         </div>
 
         <iframe
@@ -65,14 +116,18 @@ const Form = () => {
           <label>Sale</label>
         </div>
         <div className="text-field-cont">
-          <img src={saleIcon} height={"15px"} />
-          <input placeholder="50.0000"></input>
+          <img src={saleIcon} height={"15px"} alt="img-not-found" />
+          <input
+            placeholder="50.0000"
+            value={sale}
+            onChange={(e) => setSale(e.target.value)}
+          ></input>
         </div>
         <div className="label-cont">
           <label>Date & Time</label>
         </div>
         <div className="text-field-cont">
-          <img src={dateIcon} height={"15px"} />
+          <img src={dateIcon} height={"15px"} alt="img-not-found" />
           <DatePicker
             className="date-picker"
             selected={startDate}
@@ -85,17 +140,24 @@ const Form = () => {
           <label>Options</label>
         </div>
         <div className="text-field-cont">
-          <img src={optionIcon} height={"15px"} />
-          <input placeholder="select option"></input>
-          <img src={dropIcon} height={"15px"} />
+          <img src={optionIcon} height={"15px"} alt="img-not-found" />
+          <input
+            placeholder="select option"
+            value={options}
+            onChange={(e) => setOptions(e.target.value)}
+          ></input>
+          <img src={dropIcon} height={"15px"} alt="img-not-found" />
         </div>
         <div className="label-cont">
           <label>Product</label>
         </div>
         <div className="text-field-cont">
-          <img src={optionIcon} height={"15px"} />
-          <input></input>
-          <img src={dropIcon} height={"15px"} />
+          <img src={optionIcon} height={"15px"} alt="img-not-found" />
+          <input
+            value={product}
+            onChange={(e) => setProduct(e.target.value)}
+          ></input>
+          <img src={dropIcon} height={"15px"} alt="img-not-found" />
         </div>
         <div className="label-cont">
           <label>Note</label>
@@ -105,6 +167,8 @@ const Form = () => {
             placeholder="Enter note"
             type="text"
             style={{ padding: "2em" }}
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
           ></input>
         </div>
         <div className="button-cont">
